e2257e395cc4063ca883b7032ff260947e385cd6
Shan Robertson <shan@invisibled.com>
2020-12-17 12:23:41 -0800

    trying to figure out why dev isn't working

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 
 const isProd = process.env.NODE_ENV === 'production'
-const basePath = '/tailor'
+//const basePath = '/tailor'
 
 module.exports = {
   env : {
     PROJECT_NAME : 'Tailor',
     GTM_ID : 'GTM-KZV9FNK',
-    BASE_PATH : isProd ? basePath : '',
+    //BASE_PATH : isProd ? basePath : '',
 
     RECAPTCHA_SITE_KEY : '6LcI8c0ZAAAAAK77Jw_Nx5MsptdE-aLZAZtctyh4',
     RECAPTCHA_SECRET : '6LcI8c0ZAAAAAPnGy3iOBqvptWdhvlWdK8uU0Onc'
   },
 
-  basePath : isProd ? basePath : ''
+  //basePath : isProd ? basePath : ''
 }
