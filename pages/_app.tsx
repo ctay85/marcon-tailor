ccf9040f333a04eb3bcf183d65458f8c20b281a6
Shan Robertson <shan@invisibled.com>
2021-01-21 10:19:00 -0800

    making some final tweaks, pushing up to test the new more button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,20 +21,24 @@ interface ReduxProps {
 }
 
 // Component
-const App = ({ Component, pageProps, router, reduxStore } : AppProps & ReduxProps) => (
-  <Provider store={reduxStore}>
-    <Head />
-    <Header />
+const App = ({ Component, pageProps, router, reduxStore } : AppProps & ReduxProps) => {
+  const initialOpacity = router.route === '/' ? 1 : 0
 
-    <AnimatePresence exitBeforeEnter>
-      <motion.div key={ router.route } initial={{ opacity : 0 }} animate={{ opacity : 1 }} exit={{ opacity : 0 }} transition={{ duration : 0.5 }}>
-        <Component { ...pageProps } />
-      </motion.div>
-    </AnimatePresence>
+  return (
+    <Provider store={reduxStore}>
+      <Head />
+      <Header />
 
-    <Footer />
-  </Provider>
-)
+      <AnimatePresence exitBeforeEnter>
+        <motion.div key={ router.route } initial={{ opacity : initialOpacity }} animate={{ opacity : 1 }} exit={{ opacity : 0 }} transition={{ duration : 0.5 }}>
+          <Component { ...pageProps } />
+        </motion.div>
+      </AnimatePresence>
+
+      <Footer />
+    </Provider>
+  )
+}
 
 // Export
 export default withReduxStore(App)
