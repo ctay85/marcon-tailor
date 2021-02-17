82a34742ec7afb5ff729b5b7911308c2c5557e03
Shan Robertson <shan@invisibled.com>
2021-02-17 14:45:57 -0800

    removing base path

--- next.config.js
+++ next.config.js
@@ -6,11 +6,12 @@ module.exports = {
   env : {
     PROJECT_NAME : 'Tailor',
     GTM_ID : 'GTM-KZV9FNK',
-    BASE_PATH : isProd ? basePath : '',
+    //BASE_PATH : isProd ? basePath : '',
+    BASE_PATH : '',
 
     RECAPTCHA_SITE_KEY : '6LcI8c0ZAAAAAK77Jw_Nx5MsptdE-aLZAZtctyh4',
     RECAPTCHA_SECRET : '6LcI8c0ZAAAAAPnGy3iOBqvptWdhvlWdK8uU0Onc'
   },
 
-  basePath : isProd ? basePath : ''
+  //basePath : isProd ? basePath : ''
 }
